e4036e659c4c70cc7a4115c853bc72d524f776cc
Todd Norton <toddmnorton@live.com>
2020-11-09 16:00:13 -0700

    added seed file for sql

--- db/seed.sql
+++ db/seed.sql
@@ -4,3 +4,28 @@ create table if not exists users (
     password varchar(500) NOT NULL
 );
 
+create table if not exists inventory (
+    product_id serial primary key,
+    product_name varchar(100) NOT NULL,
+    product_description varchar(500) NOT NULL,
+    product_price float(4, 2),
+    product_review set(1, 2, 3, 4, 5),
+    product_location_city varchar(100),
+    product_location_state_abbreviation varchar(2),
+);
+
+create table if not exists cart (
+    order_id serial primary key,
+    product_id foreign key references inventory(product_id),
+    product_name foreign key references inventory(product_name),
+    quantity int,
+    product_price float(4, 2) foreign key references inventory(product_price),
+    final_item_cost float(6, 2),
+    total_cost float(6, 2),
+);
+
+create table if not exists order_history (
+    select user_id from users
+    union all
+    select order_id, product_name, total_cost from cart
+);
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,15 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Dashboard from './Components/Dashboard/Dashboard.js';
 import Landing from '../src/Components/Landing/Landing';
+import CustomerService from './Components/CustomerService/CustomerService.js';
+import Checkout from '../src/Components/Checkout/Checkout';
+
 
 export default (    
   <Switch>
     <Route exact path="/" component={Landing} />
     <Route path="/Dashboard" component={Dashboard} />
+    <Route path="/CustomerService" component={CustomerService} />
+    <Route path='/Checkout' component={Checkout} />
   </Switch>
 );
