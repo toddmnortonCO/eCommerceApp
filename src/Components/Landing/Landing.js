4dfcfdcc66322c87773f8b956a9e7c20ac296441
Todd Norton <toddmnorton@live.com>
2020-11-10 15:26:33 -0700

    some css on landing

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -3,33 +3,39 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { getUser } from "../../redux/reducer";
 import "./Landing.scss";
-import Header from '../Header/Header';
 
 class Landing extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       email: "",
       password: "",
     };
   }
 
+  handleInput = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
   handleRegister = (e) => {
     e.preventDefault();
-    const { email, password } = this.state;
-    axios
-      .post("/api/register", { email, password })
-      .then((res) => {
-        this.props.getUser(res.data);
-        this.props.history.push("/dashboard");
-      })
-      .catch((err) => console.log(err));
-  };
+    const { email, password } = this.state
+      axios
+        .post("/api/register", { email, password })
+        .then((res) => {
+          this.props.getUser(res.data);
+          this.props.history.push("/dashboard");
+        })
+        .catch((err) => console.log(err));
+    } 
+  
 
   handleLogin = (e) => {
     e.preventDefault();
     const { email, password } = this.state;
-    axios
+      axios
       .post("/api/login", { email, password })
       .then((res) => {
         this.props.getUser(res.data);
@@ -39,11 +45,32 @@ class Landing extends Component {
   };
 
   render() {
-    return (
-    <div>
-      <Header />
-      Hello this is the landing page
-    </div>
+      return (
+      <div className="body">
+        <h1>Welcome to Todd's Computer Emporium!</h1>
+        <br />
+        <h5 color='black'>Please Login to Shop</h5>
+        <input
+          value={this.state.email}
+          name='email'
+          placeholder="Email"
+          autocomplete="off"
+          onChange={(e) => this.handleInput(e)}
+        />
+        <br />
+        <input
+          value={this.state.password}
+          name="password"
+          placeholder="Password"
+          className="password-input"
+          autocomplete="off"
+          onChange={(e) => this.handleInput(e)}
+        />        
+        <br />
+        <button className='login-button' onClick={this.handleLogin}>Login</button>
+        <br />
+        <button className='register-button' onClick={this.handleRegister}>Register</button>
+      </div>
     );
   }
 }
