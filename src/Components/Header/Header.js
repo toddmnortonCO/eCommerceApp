5b247ebf06a0b3122e414c5afad0ae578584968c
Todd Norton <toddmnorton@live.com>
2020-11-10 17:20:50 -0700

    done for the night

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,33 +1,25 @@
-import React, {Component} from 'react';
-import { Link } from 'react-router-dom';
-import './Header.scss';
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Header.scss";
 
 class Header extends Component {
-    render() {
-        return(
-            <div>
-                <section className="navbar">
-                    <section className='flex-container'>
-                    <Link 
-                    className="link"
-                    to="/dashboard"> 
-                    Home 
-                    </Link>
-                    <Link className="link" 
-                    to="/customerService"> 
-                    Contact Customer Service 
-                    </Link>
-                    <Link className="link" 
-                    to="/orderHistory"> 
-                    Order History 
-                    </Link>
-                    </section>
-                    <button >Log Out</button>
-                </section>
+  render() {
+    return (
+      <div>
+        <section className="navbar">
+            <div className='ul'>
+            <Link className='link'
+                img="fa fa-home"
+            >
+                Home
+            </Link>
             </div>
-        )
-    }
+          <h3 className="h3">Todd's Computer Emporium</h3>
+          <button className="logout-button">Log Out</button>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Header;
