d5a7a788c082daa86e70909a758c72a5c3a5b57b
toddmnortonCO <toddmnorton@live.com>
2020-11-13 13:00:09 -0700

    working w/ nodemailer

--- server/index.js
+++ server/index.js
@@ -47,14 +47,3 @@ app.delete('/api/shoppingCart/:product_id', shoppingCartController.removeItem);
 app.post('/api/payment', stripeController.completePayment);
 
 // sendgrid endpoint
-const sgMail = require('@sendgrid/mail');
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    const msg = {
-      to: 'toddmnorton@live.com',
-      from: 'toddscomputeremporium@gmail.com',
-      subject: 'Sending with SendGrid is Fun',
-      text: 'and easy to do anywhere, even with Node.js',
-      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    };
-
-sgMail.send(msg);
