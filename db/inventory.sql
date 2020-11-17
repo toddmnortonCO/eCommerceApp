ce058134c6dd9f773fbebfd3e8fe648376ffc0ab
toddmnortonCO <toddmnorton@live.com>
2020-11-17 15:19:31 -0700

    saving

--- db/inventory.sql
+++ db/inventory.sql
@@ -1,6 +1,6 @@
-1	1	MacBook Pro	Apple M1 Chip with 8-Core CPU and 8-Core GPU, 512GB Storage	1299.00	
-2	2	MacBook Pro 16in	2.6GHz 6-Core Processor, 512GB Storage, AMD Radeon Pro 5300M	2199.00	
-3	3	Mac Mini	3.0GHz Intel Core i5 6-Core Processor with Intel UHD Graphics 630, 512GB Storage	1049.00	
-4	4	27in iMac	3.8GHz 8-Core Processor with Turbo Boost up to 5.0GHz, 512GB Storage, Retina 5K Display	2099.00	
-5	5	MacBook Air	Apple M1 Chip with 8‑Core CPU and 7‑Core GPU, 256GB Storage	899.00	
-6	6	Pro Display XDR	32-inch Retina 6K, Nano-texture glass upgrade	5599.00	
+1	MacBook Pro	Apple M1 Chip with 8-Core CPU and 8-Core GPU, 512GB Storage	1299.00	
+2	MacBook Pro 16in	2.6GHz 6-Core Processor, 512GB Storage, AMD Radeon Pro 5300M	2199.00	
+3	Mac Mini	3.0GHz Intel Core i5 6-Core Processor with Intel UHD Graphics 630, 512GB Storage	1049.00	
+4	27in iMac	3.8GHz 8-Core Processor with Turbo Boost up to 5.0GHz, 512GB Storage, Retina 5K Display	2099.00	
+5	MacBook Air	Apple M1 Chip with 8‑Core CPU and 7‑Core GPU, 256GB Storage	899.00	
+6	Pro Display XDR	32-inch Retina 6K, Nano-texture glass upgrade	5599.00	
